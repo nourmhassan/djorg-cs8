fc639d243eee854496ce7b5ab8b1e37113ffb071
nourmhassan <nour-pop1@hotmail.com>
2018-06-29 12:37:02 -0700

    added keys

--- djorg/settings.py
+++ djorg/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('C:\Users\nour\.ssh\id_rsa')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)

de3f3a10f981505441eec3010d43540ca9ac19a2
nourmhassan <nour-pop1@hotmail.com>
2018-06-29 13:36:34 -0700

    trying as env var

--- djorg/settings.py
+++ djorg/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djorg-cs8-nour.herokuapp.com','*.herokuapp.com']
 
 
 # Application definition

4d5bb9493cad16faa158bafebe6cf0f1b2dc2017
nourmhassan <nour-pop1@hotmail.com>
2018-07-09 13:57:25 -0700

    fixed  db

--- djorg/settings.py
+++ djorg/settings.py
@@ -80,14 +80,18 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# DATABASES['default'] = dj_database_url.config(default='postgres://...')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
     }
-}
-DATABASES['default'] = dj_database_url.config(default='postgres://...')
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
